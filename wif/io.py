ed3e77d18fd8b57902175cd304221aa8d3520e05
Frederic Vogels <frederic.vogels@ucll.be>
2019-08-09 15:03:29 +0200

    added comments

--- wif/io.py
+++ wif/io.py
@@ -33,18 +33,28 @@ def block_to_image(block):
     '''
     Takes a block of data, base64-decodes it and turns it into an image.
     '''
+    # Decode base64
     decoded = base64.b64decode(block)
+
+    # Read two little endian 32 bit integers
     width, height = struct.unpack('<2I', decoded[:8])
+
+    # Read groups of 3 unsigned chars
     pixels = list(struct.iter_unpack("3B", decoded[8:]))
 
+    # Create image
     image = Image.new('RGB', (width, height))
+
+    # Get pixel buffer
     image_buffer = image.load()
 
+    # Write pixels to buffer
     for y in range(height):
         for x in range(width):
             i = y * width + x
             image_buffer[x, y] = pixels[i]
 
+    # Return image
     return image
 
 
@@ -52,3 +62,4 @@ def read_frames(stream):
     for block in read_blocks(stream):
         image = block_to_image(block)
         image.save(sys.stdout.buffer, format=args.format)
+
