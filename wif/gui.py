de002772fcf80c38095b808b9ba381657a3c153b
Frederic Vogels <frederic.vogels@ucll.be>
2021-01-16 22:27:39 +0100

    added fps slider

--- wif/gui.py
+++ wif/gui.py
@@ -15,6 +15,7 @@ class Application(tk.Frame):
     def __create_variables(self):
         self.__create_index_variable()
         self.__create_animating_variable()
+        self.__create_fps_variable()
 
     def __create_animating_variable(self):
         def callback(var, idx, mode):
@@ -29,9 +30,14 @@ class Application(tk.Frame):
         self.__index = tk.IntVar(0)
         self.__index.trace_add('write', callback)
 
+    def __create_fps_variable(self):
+        self.__fps = tk.IntVar(value=30)
+
     def __create_widgets(self):
         frame_slider = tk.Scale(self, variable=self.__index, from_=0, to=len(self.__images) - 1, orient=tk.HORIZONTAL)
         frame_slider.pack()
+        fps_slider = tk.Scale(self, variable=self.__fps, from_=1, to=60, orient=tk.HORIZONTAL)
+        fps_slider.pack()
         animation_checkbox = tk.Checkbutton(self, text="Animate", variable=self.__animating)
         animation_checkbox.pack()
         self.__label = tk.Label(self)
@@ -49,4 +55,4 @@ class Application(tk.Frame):
             self.__schedule_tick()
 
     def __schedule_tick(self):
-        self.root.after(1000 // 30, self.__tick)
+        self.root.after(1000 // self.__fps.get(), self.__tick)
