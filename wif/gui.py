015441ae471a64dc2ae26627235a6bb4f1b62b0a
Frederic Vogels <frederic.vogels@ucll.be>
2021-01-16 22:20:12 +0100

    renamed local variable

--- wif/gui.py
+++ wif/gui.py
@@ -30,8 +30,8 @@ class Application(tk.Frame):
         self.__index.trace_add('write', callback)
 
     def __create_widgets(self):
-        slider = tk.Scale(self, variable=self.__index, from_=0, to=len(self.__images) - 1, orient=tk.HORIZONTAL)
-        slider.pack()
+        frame_slider = tk.Scale(self, variable=self.__index, from_=0, to=len(self.__images) - 1, orient=tk.HORIZONTAL)
+        frame_slider.pack()
         animation_checkbox = tk.Checkbutton(self, text="Animate", variable=self.__animating)
         animation_checkbox.pack()
         self.__label = tk.Label(self)
