dff4d42967078a2222487526533c68656f143c6e
Frederic Vogels <frederic.vogels@ucll.be>
2021-07-08 22:39:27 +0200

    saving scripts

--- wif/gui/studio.py
+++ wif/gui/studio.py
@@ -8,19 +8,34 @@ import wif.bgworker
 import wif.raytracer
 import wif.gui.msgview
 
+
 class EditorTab:
     def __init__(self, parent_notebook, filename, contents):
+        self.__parent_notebook = parent_notebook
         frame = tk.Frame(parent_notebook)
         frame.pack(fill=tk.BOTH, expand=True)
         self.editor = tk.Text(frame)
         self.editor.pack(fill=tk.BOTH, expand=True)
         self.editor.insert('1.0', contents)
+        self.__filename = filename
         tab_title = filename if filename else 'untitled'
         parent_notebook.add(frame, text=tab_title)
 
     @property
     def script(self):
         return self.editor.get("1.0", tk.END)
+
+    @property
+    def filename(self):
+        return self.__filename
+
+    @filename.setter
+    def filename(self, value):
+        self.__filename = value
+
+    def save(self):
+        with open(self.filename, 'w') as file:
+            file.write(self.script)
 
 
 class StudioApplication(tk.Frame):
@@ -47,6 +62,10 @@ class StudioApplication(tk.Frame):
         self.root.bind('<Control-n>', lambda event: self.__new_script())
         file_menu.add_command(label="Open file", underline=0, command=self.__open_file, accelerator="Ctrl+O")
         self.root.bind('<Control-o>', lambda event: self.__open_file())
+        file_menu.add_command(label="Save file", underline=0, command=self.__save_file, accelerator="Ctrl+S")
+        self.root.bind('<Control-s>', lambda event: self.__save_file())
+        file_menu.add_command(label="Save file as", underline=0, command=self.__save_file_as, accelerator="Alt+S")
+        self.root.bind('<Alt-s>', lambda event: self.__save_file_as())
         file_menu.add_command(label="Render", underline=0, command=self.__render_script, accelerator="F5")
         self.root.bind('<F5>', lambda event: self.__render_script())
         menubar.add_cascade(menu=file_menu, label="File", underline=0)
@@ -66,7 +85,7 @@ class StudioApplication(tk.Frame):
     def __add_editor_tab(self, filename, contents):
         tab = EditorTab(self.__notebook, filename, contents)
         self.__tabs.append(tab)
-        self.__notebook.select(len(self.__tabs) - 1)
+        self.__notebook.select(len(self.__notebook.tabs()) - 1)
 
     def __open_wif_viewer(self, path):
         blocks = wif.io.read_blocks_from_file(path)
@@ -87,6 +106,23 @@ class StudioApplication(tk.Frame):
             with open(filename) as file:
                 contents = file.read()
             self.__add_editor_tab(file.name, contents)
+
+    def __save_file(self):
+        tab = self.selected_tab
+        if tab.filename:
+            tab.save()
+        else:
+            self.__save_file_as()
+
+    def __save_file_as(self):
+        tab = self.selected_tab
+        filetypes = [('Scripts', '*.chai')]
+        filename = filedialog.asksaveasfilename(filetypes=filetypes, defaultextension='.chai')
+        if filename:
+            tab.filename = filename
+            tab.save()
+            index = self.__tabs.index(tab)
+            self.__notebook.tab(index, text=filename)
 
     @property
     def __selected_tab_index(self):
